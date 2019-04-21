8fbb7a9fc8f63ceb2ddd3330dc2dce5a339c0daa
Hugo Sandelius <hugo.sandelius@gmail.com>
2019-04-22 01:50:00 +0200

    Further work on code generation

--- src/main/scala/CodeGenerator.scala
+++ src/main/scala/CodeGenerator.scala
@@ -2,29 +2,47 @@ import SymbolTableCreator.SymbolTable
 
 object CodeGenerator {
   def generate(program: Program, symTable: SymbolTable): String = ???
-  private def genBinaryOp(binOp: BinaryOp, instruction: String) =
-    asm(gen(binOp.leftOp), gen(binOp.rightOp), instruction)
 
   private def esc(s: String) = "'" + s + "'"
 
-  private def asm(s: String*) = s.mkString("\n")
+  private def asm(s: String*) = s.mkString("\n") + "\n"
+
+  private def genBinaryOp(binOp: BinaryOp, instruction: String) =
+    asm(gen(binOp.leftOp), gen(binOp.rightOp), instruction)
 
   private def gen(expr: Expr): String = {
     expr match {
-      case p: Plus => genBinaryOp(p, "iadd")
-      case m: Minus => genBinaryOp(m, "isub")
-      case gt: GreaterThan => genBinaryOp(gt, "if_icmpgt")
-      case get: GreaterOrEqualThan => genBinaryOp(get, "if_icmpge")
-      case lt: LessThan => genBinaryOp(lt, "if_icmplt")
-      case let: LessOrEqualThan => genBinaryOp(let, "if_icmple")
-      case m: Mult => genBinaryOp(m, "imul")
-      case i: IntLit => asm("ldc ", i.value.toString)
-      case n: Not => asm(gen(n.expr), "iconst_1", "ixor")
-      case n: NewArray => asm(gen(n.arraySize), "newarray int")
+      case p: Plus =>
+        genBinaryOp(p, "iadd")
+      case m: Minus =>
+        genBinaryOp(m, "isub")
+      case gt: GreaterThan =>
+        genBinaryOp(gt, "if_icmpgt")
+      case get: GreaterOrEqualThan =>
+        genBinaryOp(get, "if_icmpge")
+      case lt: LessThan =>
+        genBinaryOp(lt, "if_icmplt")
+      case let: LessOrEqualThan =>
+        genBinaryOp(let, "if_icmple")
+      case m: Mult =>
+        genBinaryOp(m, "imul")
+      case i: IntLit =>
+        asm("ldc ", i.value.toString)
+      case n: Not =>
+        asm(gen(n.expr), "iconst_1", "ixor")
+      case n: NewArray =>
+        asm(gen(n.arraySize), "newarray int")
       case n: NewObject =>
         val typeName = n.typeName.name
         asm("new " + esc(n.typeName.name), "dup", "invokespecial " + esc(typeName + "/<init>()V"))
-
+      case a: ArrayLength =>
+        asm(gen(a.array), "arraylength")
+      case a: ArrayLookup =>
+        asm(gen(a.array), gen(a.index), "iaload")
+      case _: False =>
+        asm("iconst_0")
     }
   }
+
+  // case a: ArrayAssign => asm(gen(a.array), gen(a.index), gen(a.newValue), "iastore")
 }
