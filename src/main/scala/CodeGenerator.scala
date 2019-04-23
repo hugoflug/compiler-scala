45cf7add8228bfc7e765c0c3b73411656a1717ff
Hugo Sandelius <hugo.sandelius@gmail.com>
2019-04-23 21:26:10 +0200

    Use better pattern matching in CodeGenerator

--- src/main/scala/CodeGenerator.scala
+++ src/main/scala/CodeGenerator.scala
@@ -111,64 +111,70 @@ object CodeGenerator {
 
   private def gen(node: SyntaxTreeNode, c: Context)(label: Int): CodegenResult =
     node match {
-      case a: ArrayAssign =>
-        asm(label) <++> gen(a.array, c) <++> gen(a.index, c) <++> gen(a.newValue, c) <+> "iastore"
-      case a: Assign =>
-        val assignee = a.assignee.name
+      case ArrayAssign(array, index, newValue) =>
+        asm(label) <++> gen(array, c) <++> gen(index, c) <++> gen(newValue, c) <+> "iastore"
+
+      case Assign(Identifier(assignee), newValue) =>
         val method = c.currentMethod.get
         val methodVars = oneOf(method.locals.get(assignee), method.params.get(assignee))
-        methodVars match {
-          case Some(value) => value match {
-            case Var(_, IntType(), varNo) =>
-              asm(label) <+> "istore " + varNo
-            case Var(_, BooleanType(), varNo) =>
-              asm(label) <+> "istore " + varNo
-            case Var(_, _, varNo) =>
-              asm(label) <+> "astore " + varNo
-          }
-          case None =>
-            val clazz = c.currentClass.get
-            val type_ = clazz.fields(assignee).type_
-            val fieldDesc = clazz.name + "/" + assignee
-            val typeDesc = typeDescriptor(type_)
-            asm(label) <+> "aload_0" <+> "swap" <+> "putfield " + esc(fieldDesc) + " " + esc(typeDesc)
-        }
-      case b: Block =>
-        asm(label) <++> genAll(b.stmtList, c)
-      case m: MethodDecl =>
-        val name = m.name.name
+
+        asm(label) <++> gen(newValue, c) <++>
+          (label =>
+            methodVars match {
+              case Some(value) => value match {
+                case Var(_, IntType(), varNo) =>
+                  asm(label) <+> "istore " + varNo
+                case Var(_, BooleanType(), varNo) =>
+                  asm(label) <+> "istore " + varNo
+                case Var(_, _, varNo) =>
+                  asm(label) <+> "astore " + varNo
+              }
+              case None =>
+                val clazz = c.currentClass.get
+                val type_ = clazz.fields(assignee).type_
+                val fieldDesc = clazz.name + "/" + assignee
+                val typeDesc = typeDescriptor(type_)
+                asm(label) <+> "aload_0" <+> "swap" <+> "putfield " + esc(fieldDesc) + " " + esc(typeDesc)
+            })
+
+      case Block(stmtList) =>
+        asm(label) <++> genAll(stmtList, c)
+
+      case MethodDecl(type_, Identifier(name), argList, varDeclList, stmts, returnVal) =>
         val methodTable = c.symTable(c.currentClass.get.name).methods(name)
         val newContext = c.copy(currentMethod = Some(methodTable))
-        val methodDescr = methodDescriptor(name, m.argList, methodTable.returnType)
+        val methodDescr = methodDescriptor(name, argList, methodTable.returnType)
         val maxStack = 500 //TODO: calculate
         asm(label) <+>
           ".method public " + esc(methodDescr) <+>
           ".limit stack " + maxStack <+>
           ".limit locals " + (methodTable.params.size + methodTable.locals.size + 1) <++>
-          genAll(m.argList, newContext) <++>
-          genAll(m.varDeclList, newContext) <++>
-          genAll(m.stmts, newContext) <++>
-          gen(m.returnVal, newContext) <+>
-          (m.typeName match {
+          genAll(argList, newContext) <++>
+          genAll(varDeclList, newContext) <++>
+          genAll(stmts, newContext) <++>
+          gen(returnVal, newContext) <+>
+          (type_ match {
             case IntType() | BooleanType() => "ireturn"
             case _ => "areturn"
           }) <+>
           ".end method"
-      case w: While =>
+
+      case While(condition, stmt) =>
         val start = label
         val after = label + 1
         asm(label + 2) <+>
           start + ":" <++>
-          gen(w.condition, c) <+>
+          gen(condition, c) <+>
           "ifeq: " + after <++>
-          gen(w.stmt, c) <+>
+          gen(stmt, c) <+>
           "goto " + start <+>
           after + ":"
-      case s: Syso =>
-        val printeeType = TypeChecker.getType(s.printee, c)
+
+      case Syso(printee) =>
+        val printeeType = TypeChecker.getType(printee, c)
         asm(label) <+>
           "getstatic java/lang/System/out Ljava/io/PrintStream;" <++>
-          gen(s.printee, c) <++>
+          gen(printee, c) <++>
           (label =>
             printeeType match {
               case IntType() =>
@@ -204,48 +210,47 @@ object CodeGenerator {
         asm(label) <++> genBinaryOp(let, "if_icmple", c)
       case m: Mult =>
         asm(label) <++> genBinaryOp(m, "imul", c)
-      case i: IntLit =>
-        asm(label) <+> "ldc " <+> i.value.toString
-      case n: Not =>
-        asm(label) <++> gen(n.expr, c) <+> "iconst_1" <+> "ixor"
-      case n: NewArray =>
-        asm(label) <++> gen(n.arraySize, c) <+> "newarray int"
-      case n: NewObject =>
-        val typeName = n.typeName.name
-        asm(label) <+> "new " + esc(n.typeName.name) <+> "dup" <+> "invokespecial " + esc(typeName + "/<init>()V")
-      case a: ArrayLength =>
-        asm(label) <++> gen(a.array, c) <+> "arraylength"
-      case a: ArrayLookup =>
-        asm(label) <++> gen(a.array, c) <++> gen(a.index, c) <+> "iaload"
-      case _: False =>
+      case IntLit(value) =>
+        asm(label) <+> "ldc " <+> value.toString
+      case Not(e) =>
+        asm(label) <++> gen(e, c) <+> "iconst_1" <+> "ixor"
+      case NewArray(arraySize) =>
+        asm(label) <++> gen(arraySize, c) <+> "newarray int"
+      case NewObject(Identifier(typeName)) =>
+        asm(label) <+> "new " + esc(typeName) <+> "dup" <+> "invokespecial " + esc(typeName + "/<init>()V")
+      case ArrayLength(array) =>
+        asm(label) <++> gen(array, c) <+> "arraylength"
+      case ArrayLookup(array, index) =>
+        asm(label) <++> gen(array, c) <++> gen(index, c) <+> "iaload"
+      case False() =>
         asm(label) <+> "iconst_0"
-      case _: True =>
+      case True() =>
         asm(label) <+> "iconst_1"
-      case _: This =>
+      case This() =>
         asm(label) <+> "aload_0"
       case or: Or =>
         asm(label) <++> genShortCircuitOp(or, "ifne", c)
       case and: And =>
         asm(label) <++> genShortCircuitOp(and, "ifeq", c)
-      case call: MethodCall =>
-        val objType = TypeChecker.getType(call.obj, c).asInstanceOf[ObjectType]
-        val returnType = c.symTable(objType.name).methods(call.methodName.name).returnType
-        val argTypeList = call.args.map(TypeChecker.getType(_, c))
-        val methodDesc = methodDescriptor(objType.name, call.methodName.name, argTypeList, returnType)
-        asm(label) <++> gen(call.obj, c) <++> genAll(call.args, c) <+> "invokevirtual " + esc(methodDesc)
-      case eq: Equal =>
-        val compareInstruct = TypeChecker.getType(eq.rightOp, c) match {
+      case MethodCall(obj, methodName, args) =>
+        val objType = TypeChecker.getType(obj, c).asInstanceOf[ObjectType]
+        val returnType = c.symTable(objType.name).methods(methodName.name).returnType
+        val argTypeList = args.map(TypeChecker.getType(_, c))
+        val methodDesc = methodDescriptor(objType.name, methodName.name, argTypeList, returnType)
+        asm(label) <++> gen(obj, c) <++> genAll(args, c) <+> "invokevirtual " + esc(methodDesc)
+      case Equal(leftOp, rightOp) =>
+        val compareInstruct = TypeChecker.getType(rightOp, c) match {
           case ObjectType(_) | IntArrayType() => "if_acmpeq"
           case _ => "if_icmpeq"
         }
-        asm(label) <++> gen(eq.leftOp, c) <++> gen(eq.rightOp, c) <++> genComparison(compareInstruct)
-      case eq: NotEqual => // TODO: code duplication
-        val compareInstruct = TypeChecker.getType(eq.rightOp, c) match {
+        asm(label) <++> gen(leftOp, c) <++> gen(rightOp, c) <++> genComparison(compareInstruct)
+      case NotEqual(leftOp, rightOp) => // TODO: code duplication
+        val compareInstruct = TypeChecker.getType(rightOp, c) match {
           case ObjectType(_) | IntArrayType() => "if_acmpne"
           case _ => "if_icmpne"
         }
-        asm(label) <++> gen(eq.leftOp, c) <++> gen(eq.rightOp, c) <++> genComparison(compareInstruct)
-      case p: Parens =>
-        asm(label) <++> gen(p.expr, c)
+        asm(label) <++> gen(leftOp, c) <++> gen(rightOp, c) <++> genComparison(compareInstruct)
+      case Parens(e) =>
+        asm(label) <++> gen(e, c)
     }
 }
